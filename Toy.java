64267477aabfaf1294ff083a6f7de19db0bfb823
Olya <lolcap202@gmail.com>
2024-04-04 20:04:02 +0600

    Добавлены комментарии

--- Toy.java
+++ Toy.java
@@ -1,19 +1,19 @@
 public class Toy implements Comparable<Toy> {
-    private int id;
-    private String name;
-    private double weight;
-
+    private int id;// идентификатор игрушки
+    private String name;// имя игрушки
+    private double weight;// вес игрушки
+// Конструктор
     public Toy(int id, String name, double weight) {
         this.id = id;
         this.name = name;
         this.weight = weight;
     }
-
+// Метод сравнения
     @Override
     public int compareTo(Toy other) {
         return Double.compare(this.weight, other.weight);
     }
-
+// Переопределение метода toString для вывода информации об игрушке
     @Override
     public String toString() {
         return
